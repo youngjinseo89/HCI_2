630747ee2a3b6c4a867708c34c890e602c714fb1
Youngjin Seo <youngjinseo89@gmail.com>
2024-11-02 11:28:44 +0900

    Initial Commit

---
+++ HCI_2/HCI_2App.swift
@@ -0,0 +1,17 @@
+//
+//  HCI_2App.swift
+//  HCI_2
+//
+//  Created by 서영진 on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HCI_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
